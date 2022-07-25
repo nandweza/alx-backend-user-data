8763156fa66ad23e0f68931ff7d5dbfb9b30d77b
nandweza <allannandweza@gmail.com>
2022-07-25 21:03:16 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - 401 error by using abort()
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+  """GET /api/v1/forbidden
+  Raise:
+    - a 403 error by using abort
+  """
+  abort(403)

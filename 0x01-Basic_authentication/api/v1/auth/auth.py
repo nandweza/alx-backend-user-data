361b2b47eb19f09facd4117d1baa821b0f4091ac
nandweza <allannandweza@gmail.com>
2022-07-26 08:16:23 +0300

    executed code

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """class Auth"""
     def require_auth(self, path: str,
                      excluded_paths: List[str]) -> bool:
         """Returns False"""

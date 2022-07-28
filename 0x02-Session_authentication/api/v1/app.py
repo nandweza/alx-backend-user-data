d490b1ea57eddb96257ce9aef4d70c88c439217d
nandweza <allannandweza@gmail.com>
2022-07-28 21:17:11 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,7 +58,8 @@ def before_request():
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
@@ -68,6 +69,9 @@ def before_request():
 
     if auth.current_user(request) is None:
         abort(403)
+
+    if auth.authorization_header(request) and auth.session_cookie(request):
+        return None, abort(401)
 
 
 if __name__ == "__main__":

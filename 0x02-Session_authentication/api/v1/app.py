8e4ef53a93c2bb39a09e072d74ed991f4ec7c5e4
Nandweza Allan <63627595+nandweza@users.noreply.github.com>
2022-07-28 19:59:30 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -4,7 +4,6 @@ Route module for the API
 """
 from os import getenv
 
-from pkg_resources import require
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)

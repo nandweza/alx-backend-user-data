7b44ab61b8ebb27437d993ad7991c1d08bbabe7d
nandweza <allannandweza@gmail.com>
2022-08-03 14:10:47 +0300

    fixed trailing white spaces

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -13,24 +13,24 @@ def login():
     email = request.form.get('email')
 
     if not email:
-        return jsonify({ "error": "email missing" }), 400
-    
+        return jsonify({"error": "email missing"}), 400
+
     password = request.form.get('password')
 
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
 
     try:
         found_users = User.search({'email': email})
     except Exception:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
 
     if not found_users:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
 
     for user in found_users:
         if not user.is_valid_password(password):
-            return jsonify({ "error": "wrong password" }), 401
+            return jsonify({"error": "wrong password"}), 401
 
     from api.v1.app import auth
 
@@ -43,6 +43,7 @@ def login():
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
 
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
